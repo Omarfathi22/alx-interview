1e85842b1c1e3f0389269c7f06663fd3e4208fec
Omarfathi22 <ommarf525@gmail.com>
2024-10-19 13:42:04 +0300

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,6 +7,7 @@ needed to achieve exactly n 'H' characters in a text file using only Copy All
 and Paste operations.
 """
 
+
 def minOperations(n):
     """
     Calculate the fewest number of operations needed to result in exactly n H characters.
@@ -19,11 +20,11 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     current = 1
     factor = 1
-    
+
     while current < n:
         if n % current == 0:  # If current is a factor of n
             operations += factor  # Perform Copy All (factor) operations
@@ -32,5 +33,5 @@ def minOperations(n):
             operations += 1       # Count the Paste operation
         else:
             current += 1  # Increment current to try the next number
-    
+
     return operations
