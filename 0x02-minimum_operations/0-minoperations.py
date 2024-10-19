99dd6547f941d3cd8db84ad641dc4193e8c2d5ac
Omarfathi22 <ommarf525@gmail.com>
2024-10-19 13:52:11 +0300

    fix-pep8

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,16 +2,20 @@
 """
 Minimum Operations
 
-This module contains a function to calculate the minimum number of operations
-needed to achieve exactly n 'H' characters in a text file using only Copy All
+This module contains a function to calculate the 
+minimum number of operations
+needed to achieve exactly n 'H' characters in a text
+file using only Copy All
 and Paste operations.
 """
+
 
 
 def minOperations(n):
     """
     minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    Gets fewest # of operations needed to
+    result in exactly n H characters
     """
     
     
@@ -25,7 +29,8 @@ def minOperations(n):
             ops += root
             # set n to the remainder
             n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
+            # reduce root to find remaining smaller vals
+            # that evenly-divide n
             root -= 1
         # increment root until it evenly-divides n
         root += 1
