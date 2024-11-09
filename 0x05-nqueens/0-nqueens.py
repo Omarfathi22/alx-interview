caa1eb42761c0d43e1230f891b8426a12fb62d1c
Omarfathi22 <ommarf525@gmail.com>
2024-11-09 21:39:11 +0200

    fixed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,35 +4,22 @@
 import sys
 
 
+import sys
+
+
 def generate_solutions(row, column):
     """
     Generate all possible solutions for placing queens on an NxN chessboard.
-
-    Arguments:
-    row -- Number of rows (queens) to place
-    column -- Number of columns (board size)
-
-    Returns:
-    solution -- List of valid configurations (solutions) of queens' placements
     """
-    solution = []
+
+    solution = [[]]
     for queen in range(row):
         solution = place_queen(queen, column, solution)
     return solution
 
 
 def place_queen(queen, column, prev_solution):
-    """
-    Place a queen in a valid position in the given row.
 
-    Arguments:
-    queen -- Current row to place the queen
-    column -- Total number of columns
-    prev_solution -- List of already placed queens' positions
-
-    Returns:
-    safe_position -- List of all valid positions for the current queen
-    """
     safe_position = []
     for array in prev_solution:
         for x in range(column):
@@ -42,18 +29,6 @@ def place_queen(queen, column, prev_solution):
 
 
 def is_safe(q, x, array):
-    """
-    Check if placing a queen at position (q, x) is safe.
-    A position is safe if no queen already placed can attack the new position.
-
-    Arguments:
-    q -- Row index of the new queen
-    x -- Column index of the new queen
-    array -- List of positions of already placed queens
-
-    Returns:
-    True if the position (q, x) is safe, False otherwise
-    """
     if x in array:
         return False
     else:
@@ -61,15 +36,6 @@ def is_safe(q, x, array):
 
 
 def init():
-    """
-    Initialize and validate the input argument (N) from the command line.
-
-    Returns:
-    n -- The size of the chessboard (N)
-
-    Exits with status 1 if the input is invalid
-    (not a number, less than 4, or missing).
-    """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -85,12 +51,11 @@ def init():
 
 
 def n_queens():
-    """
-    Main function to solve the N-Queens problem and print all valid solutions.
-    """
-    n = init()
-    solutions = generate_solutions(n, n)
 
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
     for array in solutions:
         clean = []
         for q, x in enumerate(array):
